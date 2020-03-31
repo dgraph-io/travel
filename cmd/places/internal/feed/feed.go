84a91b666d959948d8a0730605ee73e9508c1a1e
Prashant Shahi <prashant@dgraph.io>
2020-03-31 20:47:51 +0530

    Adding schema and filtering results

--- cmd/places/internal/feed/feed.go
+++ cmd/places/internal/feed/feed.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/dgraph-io/dgo/v2"
+	"github.com/dgraph-io/dgo/v2/protos/api"
+	"google.golang.org/grpc"
 	"googlemaps.github.io/maps"
 )
 
@@ -30,87 +33,65 @@ func Pull(ctx context.Context, host string) error {
 		return err
 	}
 
-	data, err := json.Marshal(resp)
+	data, err := json.Marshal(resp.Results)
 	if err != nil {
 		return err
 	}
 
-	//	 {
-	//     "geometry": {
-	//         "location": {
-	//             "lat": -33.8688197,
-	//             "lng": 151.2092955
-	//         },
-	//         "location_type": "",
-	//         "bounds": {
-	//             "northeast": {
-	//                 "lat": 0,
-	//                 "lng": 0
-	//             },
-	//             "southwest": {
-	//                 "lat": 0,
-	//                 "lng": 0
-	//             }
-	//         },
-	//         "viewport": {
-	//             "northeast": {
-	//                 "lat": -33.5781409,
-	//                 "lng": 151.3430209
-	//             },
-	//             "southwest": {
-	//                 "lat": -34.118347,
-	//                 "lng": 150.5209286
-	//             }
-	//         },
-	//         "types": null
-	//     },
-	//     "name": "Sydney",
-	//     "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png",
-	//     "place_id": "ChIJP3Sa8ziYEmsRUKgyFmh9AQM",
-	//     "scope": "GOOGLE",
-	//     "types": [
-	//         "colloquial_area",
-	//         "locality",
-	//         "political"
-	//     ],
-	//     "photos": [
-	//         {
-	//             "photo_reference": "CmRaAAAAe0Qh5YfppyNenci7n5qbL8cBAY9tym9xkrFDhMBC6XzSO5dgcJQigzBv2V0WQfqP1-xNgx62oD8lLqJLxW4OonCnLIW5d4_LiteSBqc5-WnziOmiyw5BbspEMqciu5axEhAGZevIYmTdRvvQ1uSUi6LdGhQtaMoKtH7GHIemi25JhCTTUzQ8Jw",
-	//             "height": 452,
-	//             "width": 720,
-	//             "html_attributions": [
-	//                 "\u003ca href=\"https://maps.google.com/maps/contrib/115479276413292861472\"\u003eAshutosh Kumar\u003c/a\u003e"
-	//             ]
-	//         }
-	//     ],
-	//     "vicinity": "Sydney NSW, Australia",
-	//     "id": "044785c67d3ee62545861361f8173af6c02f4fae"
-	// }
-
 	fmt.Println(string(data))
 
-	// conn, err := grpc.Dial(host, grpc.WithInsecure())
-	// if err != nil {
-	// 	return err
-	// }
+	conn, err := grpc.Dial(host, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
 
-	// client := dgo.NewDgraphClient(
-	// 	api.NewDgraphClient(conn),
-	// )
+	client := dgo.NewDgraphClient(
+		api.NewDgraphClient(conn),
+	)
 
-	// txn := client.NewTxn()
+	op := &api.Operation{
+		Schema: `
+			height: int .
+			width: int .
+			id: string @index(hash)  .
+			icon: string .
+			lat: float .
+			lng: float .
+			location_type: string  .
+			name: string @index(trigram, hash) .
+			photo_reference: string @index(hash) .
+			place_id: string @index(hash) .
+			scope: string @index(hash) .
+			vicinity: string @index(trigram) .
+			types: [string] .
+			html_attributions: [string] .
 
-	// mut := api.Mutation{
-	// 	SetJson: data,
-	// }
-	// if _, err := txn.Mutate(ctx, &mut); err != nil {
-	// 	txn.Discard(ctx)
-	// 	return err
-	// }
+			location: uid .
+			bounds: uid .
+			geometry: uid @reverse .
+			photos: [uid] @reverse @count .
+			northeast: uid .
+			southwest: uid .
+			viewport: uid .
+		`,
+	}
+	if err := client.Alter(ctx, op); err != nil {
+		return err
+	}
 
-	// if err := txn.Commit(ctx); err != nil {
-	// 	return nil
-	// }
+	txn := client.NewTxn()
+
+	mut := api.Mutation{
+		SetJson: data,
+	}
+	if _, err := txn.Mutate(ctx, &mut); err != nil {
+		txn.Discard(ctx)
+		return err
+	}
+
+	if err := txn.Commit(ctx); err != nil {
+		return nil
+	}
 
 	return nil
 }
