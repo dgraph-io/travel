a361e555a49ba642a0147fc5712f93cf8fb6729d
William Kennedy <bill@ardanlabs.com>
2020-04-04 16:59:16 -0400

    supporting paged results

--- cmd/travel-data/internal/feed/feed.go
+++ cmd/travel-data/internal/feed/feed.go
@@ -2,6 +2,7 @@ package feed
 
 import (
 	"context"
+	"io"
 	"log"
 
 	"github.com/dgraph-io/travel/cmd/travel-data/internal/places"
@@ -11,21 +12,42 @@ import (
 func Pull(log *log.Logger, apiKey string, dbHost string) error {
 	ctx := context.Background()
 
-	loc := places.Location{
-		Lat:     -33.865143,
-		Lng:     151.209900,
-		Keyword: "Sydney",
-		Radius:  5000,
-	}
-	result, err := places.Retrieve(ctx, apiKey, loc)
+	// Construct a places value so we can search and store
+	// the place data we need.
+	p, err := places.New(apiKey, dbHost)
 	if err != nil {
 		return err
 	}
 
-	log.Print("place result\n", string(result))
+	// Pull all the hotels for Sydney, Australia.
+	loc := places.Location{
+		Lat:     -33.865143, // This is the lat,lng for Sydney.
+		Lng:     151.209900,
+		Keyword: "hotels",
+		Radius:  5000,
+	}
 
-	if err := places.Store(ctx, dbHost, result); err != nil {
-		return err
+	// Grab 2 pages of data for now for testing.
+	for i := 0; i < 2; i++ {
+
+		// I hate this but we need to keep this non-idiomatic error
+		// variable because an io.EOF error means we are done but
+		// we did get data back to process.
+		result, errRet := p.Retrieve(ctx, &loc)
+		if errRet != nil && errRet != io.EOF {
+			return errRet
+		}
+		log.Printf("******************** place result ********************\n\n%s\n\n", string(result))
+
+		// Store the results in Dgraph.
+		// if err := p.Store(ctx, result); err != nil {
+		// 	return err
+		// }
+
+		// If this was the last result, we are done.
+		if errRet == io.EOF {
+			break
+		}
 	}
 
 	return nil
