e4237ba9ae1e4410a8e13924b74f8eedcd8fcc5d
William Kennedy <bill@ardanlabs.com>
2020-04-13 09:49:02 -0400

    minor refactor

--- cmd/travel-data/internal/weather/weather.go
+++ cmd/travel-data/internal/weather/weather.go
@@ -1,63 +1,18 @@
 package weather
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-// Result use used to unmarshall the weather info from the API.
-// Here is the sample JSON output to parse https://gist.github.com/hackintoshrao/f55430d644634ecf72ef67a7d847fb8b
-// Struct auto-generated using https://mholt.github.io/json-to-go/
-type Result struct {
-	Coord struct {
-		Lon int `json:"lon"`
-		Lat int `json:"lat"`
-	} `json:"coord"`
-	Weather []struct {
-		ID          int    `json:"id"`
-		Main        string `json:"main"`
-		Description string `json:"description"`
-		Icon        string `json:"icon"`
-	} `json:"weather"`
-	Base string `json:"base"`
-	Main struct {
-		Temp      float64 `json:"temp"`
-		FeelsLike float64 `json:"feels_like"`
-		TempMin   float64 `json:"temp_min"`
-		TempMax   float64 `json:"temp_max"`
-		Pressure  int     `json:"pressure"`
-		Humidity  int     `json:"humidity"`
-	} `json:"main"`
-	Wind struct {
-		Speed float64 `json:"speed"`
-		Deg   float64 `json:"deg"`
-	} `json:"wind"`
-	Clouds struct {
-		All int `json:"all"`
-	} `json:"clouds"`
-	Dt  int `json:"dt"`
-	Sys struct {
-		Type    int     `json:"type"`
-		ID      int     `json:"id"`
-		Message float64 `json:"message"`
-		Country string  `json:"country"`
-		Sunrise int     `json:"sunrise"`
-		Sunset  int     `json:"sunset"`
-	} `json:"sys"`
-	Timezone int    `json:"timezone"`
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Cod      int    `json:"cod"`
-}
-
 // Weather is used to capture only the required weather information
 // from the weathe result.
 // TODO: complete this.
 type Weather struct {
 	Temperature    float64
-	MinTemperature float64
 	MinTemperature float64
 	MaxTemperature float64
 	Pressure       int
@@ -114,4 +69,49 @@ func (client *Client) GetWeather(ctx context.Context, lat float64, lng float64) 
 	// Need to construct a struct to parse and store the result.
 	log.Println(string(data))
 	return nil
+}
+
+// Result use used to unmarshall the weather info from the API.
+// Here is the sample JSON output to parse https://gist.github.com/hackintoshrao/f55430d644634ecf72ef67a7d847fb8b
+// Struct auto-generated using https://mholt.github.io/json-to-go/
+type Result struct {
+	Coord struct {
+		Lon int `json:"lon"`
+		Lat int `json:"lat"`
+	} `json:"coord"`
+	Weather []struct {
+		ID          int    `json:"id"`
+		Main        string `json:"main"`
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+	} `json:"weather"`
+	Base string `json:"base"`
+	Main struct {
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+		TempMin   float64 `json:"temp_min"`
+		TempMax   float64 `json:"temp_max"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+	} `json:"main"`
+	Wind struct {
+		Speed float64 `json:"speed"`
+		Deg   float64 `json:"deg"`
+	} `json:"wind"`
+	Clouds struct {
+		All int `json:"all"`
+	} `json:"clouds"`
+	Dt  int `json:"dt"`
+	Sys struct {
+		Type    int     `json:"type"`
+		ID      int     `json:"id"`
+		Message float64 `json:"message"`
+		Country string  `json:"country"`
+		Sunrise int     `json:"sunrise"`
+		Sunset  int     `json:"sunset"`
+	} `json:"sys"`
+	Timezone int    `json:"timezone"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Cod      int    `json:"cod"`
 }
