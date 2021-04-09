0fed8bd3954f224bfab1eadde4be35365383e2e9
William Kennedy <bill@ardanlabs.com>
2021-04-09 11:33:19 -0400

    support to print logs after test runs

--- business/data/tests/tests.go
+++ business/data/tests/tests.go
@@ -2,7 +2,9 @@
 package tests
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"testing"
@@ -25,6 +27,10 @@ const (
 // NewUnit creates a test value with necessary application state to run
 // database tests. It will return the host to use to connect to the database.
 func NewUnit(t *testing.T) (*log.Logger, string, func()) {
+	r, w, _ := os.Pipe()
+	old := os.Stdout
+	os.Stdout = w
+
 	c := docker.StartContainer(t, dbImage, dbPort)
 
 	// teardown is the function that should be invoked when the caller is done
@@ -33,6 +39,14 @@ func NewUnit(t *testing.T) (*log.Logger, string, func()) {
 		t.Helper()
 		t.Log("tearing down test ...")
 		docker.StopContainer(t, c.ID)
+
+		w.Close()
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		os.Stdout = old
+		fmt.Println("******************** LOGS ********************")
+		fmt.Print(buf.String())
+		fmt.Println("******************** LOGS ********************")
 	}
 
 	url := fmt.Sprintf("http://%s", c.Host)
